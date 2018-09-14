b6a74c530d1e8a7b1342cc2de09fb27a37d15133
Munendra Singh <munendra.s@HCL.COM>
2018-09-14 11:15:20 +0530

    add env file

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,15 +1,29 @@
 package io.pivotal.pal.tracker;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
 
-    @GetMapping("/")
+    /*@GetMapping("/")
     public  String sayHello(){
         return "Hello Gradle !!";
+    }*/
+
+    private String welcomeMessage;
+
+    public WelcomeController(
+            @Value("${welcome_message}") String welcomeMessage
+    ) {
+        this.welcomeMessage = welcomeMessage;
+    }
+
+    @GetMapping("/")
+    public String sayHello() {
+        return welcomeMessage;
     }
 
 }
